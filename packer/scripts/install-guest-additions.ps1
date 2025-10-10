396b652d9229484863ad2650869ab4460352dcca
ubuntu <ubuntu@builder.cathouse>
2025-10-10 20:37:20 +0000

    Got it to build something

---
+++ packer/scripts/install-guest-additions.ps1
@@ -0,0 +1,3 @@
+Write-Host "Installing Guest Additions..."
+msiexec /i e:\virt\virtio-win-gt-x64.msi /quiet /norestart
+Write-Host "Guest Additions installation complete."
---
+++ packer/scripts/sysprep.ps1
@@ -0,0 +1,3 @@
+Write-Host "Sysprepping system..."
+C:\Windows\System32\sysprep\sysprep.exe /oobe /generalize /shutdown
+Write-Host "Sysprep complete."
