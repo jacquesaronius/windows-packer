90c901187fd7030ee757057c0df7e4aaf792ea74
ubuntu <ubuntu@builder.cathouse>
2025-10-10 23:55:36 +0000

    I don't know what's wrong with this thing

--- packer/scripts/sysprep.ps1
+++ packer/scripts/sysprep.ps1
@@ -1,3 +1,5 @@
 Write-Host "Sysprepping system..."
-C:\Windows\System32\sysprep\sysprep.exe /oobe /generalize /shutdown
+$proc = Start-Process -FilePath "C:\Windows\System32\Sysprep\Sysprep.exe" -ArgumentList "/oobe /generalize /quit" -PassThru
+$proc.WaitForExit()
+Write-Host "Sysprep exited with code $($proc.ExitCode)"
 Write-Host "Sysprep complete."
